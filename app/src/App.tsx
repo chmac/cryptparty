b49bb113491804cb5f9745c7ec546f83f1d432e8
Callum Macdonald <git@callum-macdonald.com>
2019-05-22 22:28:48 +0200

    Remove logo and frontpage cruft.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,7 +9,6 @@ import Events from "./scenes/Events";
 
 import { createInvitation } from "./services/invitations";
 
-import logo from "./logo.svg";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -18,21 +17,7 @@ const App: React.FC = () => {
       <ApolloProvider client={apollo}>
         <Events />
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <button onClick={createInvitation}>Create Invitation</button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          <button onClick={createInvitation}>Create Invitation</button>
         </div>
       </ApolloProvider>
     </Provider>
