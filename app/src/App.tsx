cb6d2f8d0f15aebed20f976bb4c866f79a177703
Callum Macdonald <git@callum-macdonald.com>
2019-05-22 22:39:33 +0200

    Working themed material-ui.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { ApolloProvider } from "react-apollo";
+import { createStyles, withStyles, WithStyles, Theme } from "@material-ui/core";
 
 import store from "./store";
 import apollo from "./apollo";
@@ -11,12 +12,14 @@ import { createInvitation } from "./services/invitations";
 
 import "./App.css";
 
-const App: React.FC = () => {
+const App = (props: WithStyles<typeof styles>) => {
+  const { classes } = props;
+
   return (
     <Provider store={store}>
       <ApolloProvider client={apollo}>
         <Events />
-        <div className="App">
+        <div id="App" className={classes.root}>
           <button onClick={createInvitation}>Create Invitation</button>
         </div>
       </ApolloProvider>
@@ -24,4 +27,11 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    }
+  });
+
+export default withStyles(styles)(App);
