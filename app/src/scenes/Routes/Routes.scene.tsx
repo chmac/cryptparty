570625d4ade95345a37c0729da5f3aff6e6eebb4
Callum Macdonald <git@callum-macdonald.com>
2019-05-23 19:23:52 +0200

    Remove unused import.

--- app/src/scenes/Routes/Routes.scene.tsx
+++ app/src/scenes/Routes/Routes.scene.tsx
@@ -3,7 +3,6 @@ import { Router, Route } from "react-router-dom";
 import { createStyles, withStyles, WithStyles, Theme } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
 
 import history from "../../history";
 import Bar from "../Bar";
