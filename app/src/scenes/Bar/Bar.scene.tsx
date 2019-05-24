a9e719a34d1d866643a8ed24db87aa93bce126c8
Callum Macdonald <git@callum-macdonald.com>
2019-05-24 17:03:58 +0200

    Updated top bar.

--- app/src/scenes/Bar/Bar.scene.tsx
+++ app/src/scenes/Bar/Bar.scene.tsx
@@ -3,6 +3,7 @@ import { createStyles, withStyles, WithStyles, Theme } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 const Bar: React.FC<Props> = (props: Props) => {
   // const { classes } = props;
@@ -10,8 +11,11 @@ const Bar: React.FC<Props> = (props: Props) => {
   return (
     <AppBar position="static" color="default">
       <Toolbar>
-        <Typography variant="h6" color="inherit">
-          Home Burn
+        <Typography variant="h6" component="h1" color="inherit">
+          Zero Knowledge Party
+          <Typography variant="h6" component="h2" color="inherit">
+            <Box fontSize="0.8em">Encrypted parties since 2020</Box>
+          </Typography>
         </Typography>
       </Toolbar>
     </AppBar>
