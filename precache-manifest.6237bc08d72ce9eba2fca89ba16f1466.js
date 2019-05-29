9e67d410d908bd943fcfdcb0dfb7e69de00efcdd
Callum Macdonald <git@callum-macdonald.com>
2019-05-29 17:16:27 +0200

    Updates

---
+++ precache-manifest.6237bc08d72ce9eba2fca89ba16f1466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515f2af4d31c3058937aee2beb890d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141a262ffa3c24a2b967",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "08bf741c3a70acd190a3",
+    "url": "/static/js/2.39ad7636.chunk.js"
+  },
+  {
+    "revision": "141a262ffa3c24a2b967",
+    "url": "/static/js/main.2662746b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
