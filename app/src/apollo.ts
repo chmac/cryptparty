536b64eadb045095ed45d10c683dd2aa9baf8d72
Callum Macdonald <git@callum-macdonald.com>
2019-05-29 18:03:34 +0200

    Prefix vars with `REACT_APP_`.

--- app/src/apollo.ts
+++ app/src/apollo.ts
@@ -1,6 +1,7 @@
 import ApolloClient from "apollo-boost";
 
-const uri = process.env.GRAPHQL_URL || "http://localhost:4000/graphql";
+const uri =
+  process.env.REACT_APP_GRAPHQL_URL || "http://localhost:4000/graphql";
 
 const client = new ApolloClient({
   uri
