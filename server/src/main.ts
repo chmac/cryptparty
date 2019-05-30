96e5475b52727d3e8561a5b918c0f53255913c0a
Callum Macdonald <git@callum-macdonald.com>
2019-05-30 16:01:46 +0200

    Log shutdown reason.

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,15 @@ import Server from "./server";
 
 Server.start();
 
-const shutdown = done => {};
+const shutdown = (reason: string, done) => {
+  console.log("Sutting down server #RbycQQ", reason);
+  done();
+};
 
 // Nodemon
-process.on("exit", shutdown.bind(null, process.exit));
-process.on("SIGINT", shutdown.bind(null, process.exit));
-process.on("uncaughtException", shutdown.bind(null, process.exit));
+process.on("exit", shutdown.bind(null, "exit", process.exit));
+process.on("SIGINT", shutdown.bind(null, "SIGINT", process.exit));
+process.on(
+  "uncaughtException",
+  shutdown.bind(null, "uncaughtException", process.exit)
+);
